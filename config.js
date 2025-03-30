db83aa8e8e91a55ecff532bfc9594b2598416854
Leo Kim <imleokkkk@naver.com>
2025-03-30 21:51:45 +0900

    delete : delete old files

--- config.js
+++
@@ -1,5 +0,0 @@
-const CONFIG = {
-	BACKEND_URL: "http://localhost:8080/api",
-};
-
-export default CONFIG;
--- profile.js
+++
@@ -1,3 +0,0 @@
-window.addEventListener("DOMContentLoaded", () => {
-	const profilePic = document.getElementById("profilePic");
-});
